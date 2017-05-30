ec10669d552bc00f00b8d81c7291b005208c32ce
Ooi Ghee Hong <ooigheehong@hotmail.com>
2017-05-30 10:19:10 +0800

    initial

---
+++ app/src/main/java/my/edu/tarc/practical1/MainActivity.java
@@ -0,0 +1,27 @@
+package my.edu.tarc.practical1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewMessage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Display UI
+        //R= resource
+        setContentView(R.layout.activity_main);
+
+        textViewMessage = (TextView) findViewById(R.id.textViewMessage);
+    }
+
+    public void showMessage(View v){
+        textViewMessage.setText("Hi my name is Ooi Ghee Hong, Victor");
+
+    }
+}
